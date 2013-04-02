c160b211db62a47b4e8bea0c022a2c3992660595
levibalint <levi.balint@altom.ro>
2013-04-02 18:16:14 +0300

    Update events.py
    removed space after import

--- pages/events.py
+++ pages/events.py
@@ -6,7 +6,7 @@
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from pages.base import  Base
+from pages.base import Base
 
 
 class Events(Base):
